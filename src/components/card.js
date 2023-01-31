c35c8e8fc51ef7abb2d2b6aa6134a295a8a125ce
Jack Graymer <jackgraymer@gmail.com>
2023-01-31 18:59:13 +0100

    Implements the shuffle of card images randomly

--- src/components/card.js
+++ src/components/card.js
@@ -1,31 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './card.css'
 
-function importAll(r){
-    return r.keys().map(r);
-}
-
-const images = importAll(require.context('./img', false, /\.(png||jpg||gif)$/))
-
-//console.log(images)
-
-export default function Card (){
+export default function Card (props){
     let cards = []
     for(let i=0; i<12; i++){
-        //console.log('yeet')
         cards.push(i)        
     }
-    
-    //console.log(cards)
-    cards.map((object => {
+
+    props.state.map((object => {
     }))
         return(
             <div className="cards">
-                {images.map((object => 
-                <div className="card" key={object}>
+                {props.state.map((object => 
+                <div className="card" key={object.id} onClick={props.shuffle}>
                     
-                        <img src={object}></img>
-                    <h4>{object[14] + object[15]} </h4>
+                        <img src={object.id} alt='food'></img>
+                    {<h4>{object.id[14] + object.id[15]} </h4>}
                                         
                 </div>
             ))}
---
+++ src/components/main.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import Card from './card'
+import './card.css'
+
+function importAll(r){
+    return r.keys().map(r);
+}
+
+const images = importAll(require.context('./img', false, /\.(png||jpg||gif)$/))
+const deck = images.map(img => ({id:img, clicked:false}))
+
+export default function Main () {
+    const[score, setScore] = useState([0,1,2,3,4,5])
+    const [best, setBest] = useState(score)
+
+    const [state, setState] = useState(deck)
+    console.log(state)
+    console.log(score)
+
+    let shuffled = (e) => {
+        let mix = [...state]
+        mix = mix.sort(()=> Math.random() -0.5)
+        mix = mix.sort(()=> Math.random() -0.5)
+        mix = mix.sort(()=> Math.random() -0.5)
+        setState(mix);
+    }
+
+    let changescore =() =>{
+        /* setScore(score.sort(()=> Math.random() -0.5))
+        setScore([...score,score[1]+1]) */
+        console.log(score)
+    }
+
+
+    return(
+        <div className='main' onClick={() =>changescore()}>
+            <div className='top'>
+                <h2>Score: {score}</h2>
+                <h2>Best Score: {best}</h2>
+            </div>
+            <div className='center' >
+                <Card shuffle={shuffled} state={state}></Card>
+            </div>
+      </div>
+    )
+}
